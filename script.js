b61b534cf7e2bbedab8f8524c8819806b01d9dd0
stanislike <s.joulah@outlook.fr>
2024-08-03 20:03:51 +0200

    finished

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-// Créer un événement au scroll
+let isAtTheBottom =
+  window.innerHeight + window.scrollY >= document.body.offsetHeight;
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+let scrollValue =
+  ((window.innerHeight + window.scrollY) / document.body.offsetHeight) * 100;
 
-// Faire apparaitre l'image de la partie improvise
+let playOnce = true;
 
-// Faire apparaitre la popup quand on est en bas du site
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.style.height = "60px";
+  } else if (window.scrollY === 0) {
+    navbar.style.height = "90px";
+  }
+
+  if (scrollValue >= 45) {
+    imgImprovise.style.opacity = "1";
+    imgImprovise.style.transform = "none";
+  }
+
+  if (isAtTheBottom && playOnce) {
+    popup.style.opacity = "1";
+    popup.style.transform = "none";
+  } else {
+    popup.style.opacity = "0";
+    popup.style.transform = "translate(400px)";
+  }
+});
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = "0";
+  popup.style.transform = "translate(400px)";
+  playOnce = false;
+});
